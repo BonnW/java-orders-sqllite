de9aaf3a761ee60dd1f6ee9e985415d7c9096de3
bonnw <pwonghansa@gmail.com>
2019-02-21 22:42:21 -0600

    created Customers class

---
+++ src/main/java/com/burahan/sqlliteorders/models/Customers.java
@@ -0,0 +1,146 @@
+package com.burahan.sqlliteorders.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="customers")
+public class Customers
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private long custcode;
+
+    @Column(nullable = false)
+    private String custname;
+
+    private String custcity;
+
+    private String workingarea;
+
+    private String custcountry;
+
+    private String grade;
+
+    private double openingamt;
+
+    private double receiveamt;
+
+    private double outstandingamt;
+
+    private String phone;
+
+    // Probably not correct
+    @ManyToOne
+    @JoinColumn(name = "agents", nullable = false)
+    @JsonIgnore
+    private long agentcode;
+    // ^^^^
+
+
+    public Customers()
+    {
+    }
+
+    public long getCustcode()
+    {
+        return custcode;
+    }
+
+    public long getAgentcode()
+    {
+        return agentcode;
+    }
+
+    public String getCustname()
+    {
+        return custname;
+    }
+
+    public void setCustname(String custname)
+    {
+        this.custname = custname;
+    }
+
+    public String getCustcity()
+    {
+        return custcity;
+    }
+
+    public void setCustcity(String custcity)
+    {
+        this.custcity = custcity;
+    }
+
+    public String getWorkingarea()
+    {
+        return workingarea;
+    }
+
+    public void setWorkingarea(String workingarea)
+    {
+        this.workingarea = workingarea;
+    }
+
+    public String getCustcountry()
+    {
+        return custcountry;
+    }
+
+    public void setCustcountry(String custcountry)
+    {
+        this.custcountry = custcountry;
+    }
+
+    public String getGrade()
+    {
+        return grade;
+    }
+
+    public void setGrade(String grade)
+    {
+        this.grade = grade;
+    }
+
+    public double getOpeningamt()
+    {
+        return openingamt;
+    }
+
+    public void setOpeningamt(double openingamt)
+    {
+        this.openingamt = openingamt;
+    }
+
+    public double getReceiveamt()
+    {
+        return receiveamt;
+    }
+
+    public void setReceiveamt(double receiveamt)
+    {
+        this.receiveamt = receiveamt;
+    }
+
+    public double getOutstandingamt()
+    {
+        return outstandingamt;
+    }
+
+    public void setOutstandingamt(double outstandingamt)
+    {
+        this.outstandingamt = outstandingamt;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+}
