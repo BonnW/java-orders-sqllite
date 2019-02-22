598d7f957c208d690584e987b8bd7267bbf69d88
bonnw <pwonghansa@gmail.com>
2019-02-21 21:46:42 -0600

    initialized project with IntelliJ

---
+++ src/main/java/com/burahan/sqlliteorders/SqlliteordersApplication.java
@@ -0,0 +1,15 @@
+package com.burahan.sqlliteorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlliteordersApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SqlliteordersApplication.class, args);
+    }
+
+}
